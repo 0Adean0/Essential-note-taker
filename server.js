5939076015df47c042728085c49997aac344ddfd
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-24 19:28:09 -0400

    should be finished

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const { writeFile, readFile } = require("fs")
-const notes = require("./Develop/db/db.json")
+const notes = require("./db/db.json")
 const { v4: uuidv4 } = require('uuid')
 const PORT = process.env.PORT || 3001
 const app = express()
@@ -11,12 +11,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
 app.get("/notes", (req, res) =>
- res.sendFile(path.join(__dirname, "/Develop/public/notes.html")))
+ res.sendFile(path.join(__dirname, ".public/notes.html")))
 
 
 app.get("/api/notes", (req, res) => {
     console.info(`${req.method} desire to view previous notes recieved`)
-    readFile("./Develop/db/db.json", "utf-8", (err, data) => {
+    readFile(".db/db.json", "utf-8", (err, data) => {
         err? console.log(err): res.json(JSON.parse(data))
     })
     console.log(userInputs)
@@ -29,21 +29,19 @@ app.post("/api/notes", (req, res) => {
         const newUserInput ={
             title, text,id: uuidv4(),
         } 
-        readFile(`/Develop/db/db.json`, "utf-8", (err,data) => {
+        readFile(`.db/db.json`, "utf-8", (err,data) => {
     if(err){
         throw err
     }
     const dataString = JSON.parse(data)
     dataString.push(newUserInput)
-    writeFile(`/Develop/db/db.json`, JSON.stringify(dataString, null,4), (err) => err? console.error(err):
-    console.log("notes written to JSON")
+    writeFile(`.db/db.json`, JSON.stringify(dataString, null,4), (err) => err? console.error(err):
+    console.info("notes written to JSON")
     )
     })
-    const response={
-        status:"complete",
-        body:newUserInput,
-    }
     }
 })
 app.get("*",(req, res) =>
-res.sendFile(path.join(__dirname,"/Develop/.public/index.html")))
+res.sendFile(path.join(__dirname,".public/index.html")));
+
+app.listen(PORT, () => console.log(`listening at http://localhost:${PORT}`));
