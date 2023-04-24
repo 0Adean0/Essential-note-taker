4cc5d977a2141016ccf546d34551bbbff32262d9
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-24 16:04:01 -0400

    added the app.use laws

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const path = require("path")
-const {writeFile, readFile} = require("fs")
+const { writeFile, readFile } = require("fs")
 const notes = require("./Develop/db/db.json")
-const PORT = process.env.PORT ||3001
+const PORT = process.env.PORT || 3001
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
