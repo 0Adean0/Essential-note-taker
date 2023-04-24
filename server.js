2326c961872f08fc23b06c1652170b962177c288
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-04-24 15:33:01 -0400

    added constraints to begin the server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require("express")
+const path = require("path")
+const {writeFile, readFile} = require("fs")
+const notes = require("./Develop/db/db.json")
+const PORT = process.env.PORT ||3001
+const app = express()
